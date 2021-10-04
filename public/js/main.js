ccaa9cfaa36dda83f92f295a2d8bda312cc54804
Stackbit Code Editor <projects@stackbit.com>
2021-10-04 03:14:24 +0000

    main.js: changed

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,7 @@ fetch(templatesUrl)
     .then(response => response.text())
     .then((htmlTemplate) => {
       const fragment = document.createElement('div');
-      let html = htmlTemplate.replace(MTE2MWZhMTYtZTU3NS00MDBjLTlhYjYtNGE3ZWNiNzc3YjJkNjM3NTc5NDkyMDYzMzA0NTQ0), apiKey);
+      let html = htmlTemplate.replace(/__SNIPCART_API_KEY__/g, apiKey);
       fragment.innerHTML = html;
       document.body.appendChild(fragment);
 
